a287375b232cc7ca6276a9dae0f586694aba3c2a
Mateusz Sapala <sapala.mateusz@gmail.com>
2024-02-09 19:52:07 +0100

    Add pom versions

--- src/test/kotlin/sapala/s2sauthservice/S2sAuthServiceApplicationTests.kt
+++ src/test/kotlin/sapala/s2sauthservice/S2sAuthServiceApplicationTests.kt
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class S2sAuthServiceApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
